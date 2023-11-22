9aa81768acf9764309d9f9372151d7190fc2ca42
inasfthnh <61815171+inasfthnh@users.noreply.github.com>
2023-11-22 13:06:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,12 +50,12 @@ def main():
                     """
                  )
         st.subheader('Final Project Fantastic Four')
-	    st.divider()
+	st.write("---")
         choice = st.selectbox("Menu", menu)
 
     if choice == 'Home':
         st.subheader("Welcome to Homepage")
-	    st.divider()
+	st.write("---")
         st.markdown(desc_temp)
     elif choice == "Machine Learning":
         # st.subheader("Welcome to Machine learning")
